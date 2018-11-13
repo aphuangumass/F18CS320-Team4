fa8341f79441ec121335678042e60b440b32f89f
Steven Tran <stevenlaitran@gmail.com>
2018-11-13 09:47:33 -0500

    enter

--- teamfour/src/App.js
+++ teamfour/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Table from './App_JS_snippet';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+          <Table />
       </div>
     );
   }
