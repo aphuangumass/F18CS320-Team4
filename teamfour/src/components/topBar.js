8cb5617c1cac7a0afad20aeb5bed1253bc10037d
Tomgeo64 <tomgeo64@gmail.com>
2018-12-06 13:56:27 -0500

    namechange

--- teamfour/src/components/topBar.js
+++ teamfour/src/components/topBar.js
@@ -37,7 +37,7 @@ export default class Example extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">Infosight FileBrowser</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
