30af70ce9e42242b4ca28705706c4c3812aa8dbc
Tomgeo64 <tomgeo64@gmail.com>
2018-12-09 16:41:05 -0500

    undid last change
    oops

--- teamfour/src/components/topBar.js
+++ teamfour/src/components/topBar.js
@@ -16,16 +16,7 @@ import {
   DropdownItem } from 'reactstrap';
 
 export default class Example extends React.Component {
-  Navbar.propTypes = {
-    light: PropTypes.bool,
-    dark: PropTypes.bool,
-    fixed: PropTypes.string,
-    color: PropTypes.string,
-    role: PropTypes.string,
-    expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
-    // pass in custom element to use
-  }
+  
   constructor(props) {
     super(props);
 
