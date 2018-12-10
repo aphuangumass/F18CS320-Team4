6e240888bc2b859014988bd199089e0a0473ca31
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2018-12-09 22:52:46 -0500

    1st npm install for react scripts

--- client/client/src/components/layout/Table.js
+++ client/client/src/components/layout/Table.js
@@ -22,7 +22,32 @@ function handleButtonClick (e, row) {
 
 class Table extends Component {
   state ={
-    dbData: []
+    dbData: [],
+    query: '',
+    results: []
+  }
+
+  
+  getInfo = () => {
+    axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.data
+        })
+      })
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      } else if (!this.state.query) {
+      }
+    })
   }
 
   render() {
@@ -63,6 +88,14 @@ class Table extends Component {
 
     return (
           <div>
+              <form>
+                <input
+                  placeholder="Search for..."
+                  ref={input => this.search = input}
+                  onChange={this.handleInputChange}
+                  />
+                <p>{this.state.query}</p>
+              </form>
               <ReactTable
                 data={this.state.dbData}
                 columns={columns}
