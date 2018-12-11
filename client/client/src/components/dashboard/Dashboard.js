ffe5280f27d99627dde19aef2cfe7ec215e01dee
Andrew Smith <andrewsmith@Andrews-MacBook-Air.local>
2018-12-10 20:09:22 -0500

    Added tree view, but it is currently broken

--- client/client/src/components/dashboard/Dashboard.js
+++ client/client/src/components/dashboard/Dashboard.js
@@ -9,6 +9,7 @@ import { logoutUser } from "../../actions/authActions";
 import Table from "../layout/Table";
 import { unwatchFile } from 'fs';
 
+
 class Dashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
@@ -21,33 +22,10 @@ return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-              <b>Full info</b> {user.tenant.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
           </div>
         </div>
         <div>
-          <Table tenant={user.tenant} />
+          <Table tenant={user.tenant}/>
         </div>
       </div>
       
--- client/client/src/components/layout/Table.js
+++ client/client/src/components/layout/Table.js
@@ -3,6 +3,9 @@ import ReactTable from "react-table";
 //import ReactJson from 'react-json-view';
 import axios from 'axios';
 import "react-table/react-table.css";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ReactJson from 'react-json-view';
 
 //handles button click
 //grabs correct json from row and converts it to a string
@@ -10,30 +13,33 @@ import "react-table/react-table.css";
 //gives blob url
 //makes a tempLink object so file can be given name/fileExtension
 //clicks tempLink to download file
-function handleButtonClick (e, row) {
+function handleDownloadClick (e, row) {
   var json = JSON.stringify(row.row._original.content, null, ' ');
   var blob = new Blob([json], {type: "octet/stream"});
   var url  = window.URL.createObjectURL(blob);
   var tempLink = document.createElement('a');
   tempLink.href = url;
-  tempLink.setAttribute('download', row.row._original.name);
+  tempLink.setAttribute('download', 'filename.json');
   tempLink.click();
 }
 
 class Table extends Component {
   state ={
-    dbData: []
+    dbData: [],
+    treeJSON: ""
+  }
+  handleViewClick (e, row) {
+    var json = JSON.stringify(row.row._original.content, null, ' ');
+    this.setState ({treeJSON: json}, () => {console.log("Viewing new JSON")});
   }
 
   render() {
     const tenant = '' + this.props.tenant
 
-    axios.get('http://localhost:5000/api/items/tenants/' + tenant)
+    axios.get('http://localhost:5000/api/items/')
     .then(res => this.setState({
       dbData: res.data
     }))
-
-    console.log(this.state.dbData[0])
 
     var dateOptions = {year: "numeric", month: "short", day: "numeric"};
 
@@ -50,19 +56,31 @@ class Table extends Component {
       Header: 'Date',
       id: 'date',
       accessor: d => new Date(d.date).toLocaleDateString('en-US', dateOptions)
+    },{
+      Header: 'View',
+      accessor: 'view-content',
+      //creates a new component inside of this column in the table
+      Cell : row => (
+        //button calls its clicky function when clicked
+        <button onClick={(e) => this.handleViewClick(e, row)} >View</button>
+      )
     }, {
       Header: 'Download',
       accessor: 'content',
       //creates a new component inside of this column in the table
       Cell : row => (
         //button calls its clicky function when clicked
-        <button onClick={(e) => handleButtonClick(e, row)}>Download</button>
+        <button onClick={(e) => handleDownloadClick(e, row)}>Download</button>
       )
     }]
 
 
     return (
           <div>
+              <h5>File TreeView</h5>
+              <div>
+              <ReactJson src={this.treeJSON}/>
+              </div>
               <ReactTable
                 data={this.state.dbData}
                 columns={columns}
@@ -74,5 +92,4 @@ class Table extends Component {
 
   }
 }
-
 export default Table;
