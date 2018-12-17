1ab1e1fa160ad0c3df2591394ac0e09c897f7b0b
Andrew Smith <andrewsmith@Andrews-Air.hsd1.ma.comcast.net>
2018-12-17 13:50:36 -0500

    Updated dash

--- client/client/src/components/dashboard/Dashboard.js
+++ client/client/src/components/dashboard/Dashboard.js
@@ -58,18 +58,6 @@ return (
               {/* </button> */}
               {/* </form> */}
             </div>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
           <Table tenant={user.tenant} search={this.state.value}/>
         </div>
     );
