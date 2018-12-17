04507e8f87cb525043907b4d1d11d73b2572b160
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2018-12-17 01:16:38 -0500

    Changed system storage space to icon

--- client/client/src/components/layout/Table.js
+++ client/client/src/components/layout/Table.js
@@ -10,6 +10,7 @@ import "react-table/react-table.css";
 //gives blob url
 //makes a tempLink object so file can be given name/fileExtension
 //clicks tempLink to download file
+
 function handleButtonClick (e, row) {
   var json = JSON.stringify(row.row._original.content, null, ' ');
   var blob = new Blob([json], {type: "octet/stream"});
@@ -64,46 +65,52 @@ class Table extends Component {
 
     const columns = [{
       Header: 'Serial Number',
-      accessor: 'serial'
+      accessor: 'serial',
+      width: 150,
+      resizable: false
     }, {
       Header: 'Company Name',
-      accessor: 'company'
+      accessor: 'company',
+      width: 150,
+      resizable: false
     }, {
       Header: 'Model',
-      accessor: 'fullModel'
+      accessor: 'fullModel',
+      resizable: false
     }, {
       Header: 'Date',
       id: 'date',
+      resizable: false,
       accessor: d => new Date(d.date).toLocaleDateString('en-US', dateOptions)
     }, {
       // var per = Math.floor((1 - c.capacity[0] / c.capacity[1]) * 100,
       Header: 'Capacity used',
       accessor: 'capacity',
+      width: 150,
+      resizable: false,
       sortMethod: (a, b) => {
         // console.log(Number(a[0]),b[1])
         return ((a[0] / a[1]) > (b[0] / b[1])) ? 1 : -1;
       },
-      Cell: row => (
-        <div
-          style={{
-            width: '100%',
-            height: '50%'
-          }}
-          >
-         {Math.floor((1 - row.value[0] / row.value[1]) * 100)}%
-          <div
+      Cell: row => {
+        if (Math.floor((1 - row.value[0] / row.value[1]) * 100) > 70)
+          return (
+          <div>
+          {Math.floor((1 - row.value[0] / row.value[1]) * 100)}%
+          <img src={require("./900px-GHS-pictogram-exclam.png")} class="right" 
             style={{
-              width: `${Math.floor((1 - row.value[0] / row.value[1]) * 100)}%`,
-              height: '50%',
-              backgroundColor: Math.floor((1 - row.value[0] / row.value[1]) * 100) < 70 ? '#85cc00'
-                // : Math.floor((1 - row.value[0] / row.value[1]) * 100) > 33 ? '#ffbf00'
-                : '#ff2e00',
-              borderRadius: '2px',
-              transition: 'all .2s ease-out'
+              width:'15%',
+              height: '15%'
             }}
-          />
-        </div>
-      )
+        ></img>
+        </div>)
+        else
+          return (
+          <div>
+          {Math.floor((1 - row.value[0] / row.value[1]) * 100)}%
+          </div>)      
+      }
+        
       // accessor: c => {
       //   var per = Math.floor((1 - c.capacity[0] / c.capacity[1]) * 100)
       //   return ((per >= 70) ? 
@@ -113,6 +120,7 @@ class Table extends Component {
     }, {
       Header: 'Download',
       accessor: 'content',
+      resizable: false,
       //creates a new component inside of this column in the table
       Cell : row => (
         //button calls its clicky function when clicked
