e1541e7caab91e4ff4dbdbf76b52f695fa5ca35e
Eden Mendel <mendeleden@gmail.con>
2018-12-19 17:09:37 -0500

    Update Navbar.js
    adding login logout functionality

--- client/client/src/components/layout/Navbar.js
+++ client/client/src/components/layout/Navbar.js
@@ -1,8 +1,54 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentUser } from "../../actions/authActions";
+import { logoutUser } from "../../actions/authActions";
+
 class Navbar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user : ''
+      // search: ''
+    };
+    
+    this.handleChange = this.handleChange.bind(this);  // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.auth.isAuthenticated)
+      {
+        this.setState({
+               user: nextProps.auth.user.name
+        })
+      }
+    }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  
+  
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   render() {
 
+
+    let button;
+    const { user } = this.props.auth;   //getting user
+    const {isAuthenticated} = this.props.auth;  //checking if user is logged in 
+
+    if (isAuthenticated) {
+      button = <LogoutButton onClick={this.onLogoutClick} class="waves-effect waves-light btn-small"/>;
+    } 
+    else {
+      button = null;
+    }
+    
+    
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
@@ -11,10 +57,57 @@ class Navbar extends Component {
             <img src={require("./hpelogo2_resize.png")} alt="logo" class ="left" hspace="180" vspace="10"></img>
           </Link>
 
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+             <li>  <Greeting user= {user} isLoggedIn={isAuthenticated} /></li>
+             <li> {button } </li>
+          </ul>
           </div>
         </nav>
       </div>
     );
   }
 }
-export default Navbar;
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting name={props.user.name}/>;
+  }
+  return <GuestGreeting />;
+}
+
+function UserGreeting(props) {
+  return  <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li> Hello, </li>
+              <li> {props.name} </li>
+          </ul>;
+}
+
+function GuestGreeting(props) {
+  return <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li> <Link to="/register"> Register </Link> </li>
+          <li> <Link to="/login"> Login </Link> </li>
+        </ul>;
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+
+Navbar.propTypes = {
+  getCurrentUser: PropTypes.func.isRequired,
+  logoutUser : PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { getCurrentUser, logoutUser }, 
+)(Navbar);
