6c722349a9ed511f2e7f4aae218b864dcbe95d13
Tomgeo64 <tomgeo64@gmail.com>
2018-12-19 16:58:32 -0500

    Revert "simple css to fix pagination size"
    This reverts commit 3585265e30bf8bc939a9c531de40235fbb8fba9b.

--- client/client/src/components/layout/Table.js
+++ client/client/src/components/layout/Table.js
@@ -3,11 +3,10 @@ import ReactTable from "react-table";
 //import ReactJson from 'react-json-view';
 import axios from 'axios';
 import "react-table/react-table.css";
-import ReactJson from 'react-json-view';
+import ReactJson from 'react-json-view'
 import { Container, Row, Col } from 'react-grid-system';
-import TableReadMe from '../layout/TableReadMe';
-import ReactTooltip from 'react-tooltip';
-import "./Table.css";
+import TableReadMe from '../layout/TableReadMe'
+import ReactTooltip from 'react-tooltip'
 
 
 
@@ -156,7 +155,6 @@ class Table extends Component {
               data={this.searchWithin(filter)}
               columns={columns}
               defaultPageSize = {10}
-              
             />
           </Col>
           { !this.state.showTree ? <TableReadMe name={name}/>: 
@@ -187,5 +185,4 @@ const treeHideButtonWrapper = {
   padding: '0 0.5rem',
   marginBottom: '5px'
 }
-
 export default Table;
